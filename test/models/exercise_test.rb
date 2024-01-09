cbc3a16f618ba03a3111cc55c07ec14bf0b40c9a
Mehdi Salemi <mehdisalemi@Mehdis-MacBook-Air.local>
2024-01-09 10:00:01 -0500

    Tests

--- test/models/exercise_test.rb
+++ test/models/exercise_test.rb
@@ -14,5 +14,23 @@ module Workouts
       assert_equal @exercise.id, workout_exercise.exercise_id
       assert_equal @goal, workout_exercise.goal
     end
+
+    test 'updates the goal of the exercise' do
+      workout_exercise = Workouts::Exercise.new(exercise_id: @exercise.id, goal: @goal)
+      new_goal = { reps: 12, weight: 250, repeats: 4 }
+
+      workout_exercise.update_goal(goal: new_goal)
+
+      assert_equal new_goal, workout_exercise.goal
+    end
+
+    test 'raises an error for invalid goal' do
+      workout_exercise = Workouts::Exercise.new(exercise_id: @exercise.id, goal: @goal)
+      invalid_goal = { reps: -1, weight: 250 } # Missing :repeats and invalid :reps
+
+      assert_raises(RuntimeError) do
+        workout_exercise.update_goal(goal: invalid_goal)
+      end
+    end
   end
 end
