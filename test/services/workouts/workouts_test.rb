d56c4f0a6c26f9c1a9bb1f31d5036edf65fe4239
Mehdi Salemi <mehdisalemi@Mehdis-MacBook-Air.local>
2024-01-09 09:34:56 -0500

    nits 2

--- test/services/workouts/workouts_test.rb
+++ test/services/workouts/workouts_test.rb
@@ -16,23 +16,33 @@ module Workouts
         assert_equal [], workout.exercises
     end
 
-    test 'adds exercise to workout and updates properties' do
+    test 'adds exercise to workout' do
         workout = Workouts::Workout.new
-        
+      
         assert_equal [], workout.exercises
-
+      
         ::Exercise.create!(title: "Bench Press")
-
+      
         exercise = Workouts::Exercise.new(exercise_id: "1", goal: @goal)
         workout.add_exercise(exercise: exercise)
-
+      
         assert_equal 1, workout.exercises.length
-
-        new_goal = { reps: 1, weight: 315, repeats: 1 }
-
-        workout.update_exercise(workout_exercise_id: exercise.id, goal: new_goal)
-
-        assert_equal new_goal, workout.exercises.first.goal
+        assert_equal "1", workout.exercises.first.exercise_id
+        assert_equal @goal, workout.exercises.first.goal
     end
+
+    test 'updates properties of an exercise in the workout' do
+        workout = Workouts::Workout.new
+        ::Exercise.create!(title: "Bench Press")
+        exercise = Workouts::Exercise.new(exercise_id: "1", goal: @goal)
+        workout.add_exercise(exercise: exercise)
+      
+        new_goal = { reps: 1, weight: 315, repeats: 1 }
+        workout.update_exercise(workout_exercise_id: exercise.id, goal: new_goal)
+      
+        assert_equal 1, workout.exercises.length
+        assert_equal new_goal, workout.exercises.first.goal
+      end
+
   end
 end
